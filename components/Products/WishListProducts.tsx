f2e344678aeb2ca602a324db2db2a33dc0f673fb
amitpauldev <amitpaul.per@gmail.com>
2026-07-04 01:38:44 +0600

    fixed empty products in wishlist

--- components/Products/WishListProducts.tsx
+++ components/Products/WishListProducts.tsx
@@ -1,6 +1,7 @@
 import { getLikedProductIds, getLikedProducts } from "@/lib/likes/like-queries";
 import { auth } from "@clerk/nextjs/server";
 import ProductCard from "./ProductCard";
+import EmptyState from "../common/EmptyState";
 
 const WishListProducts = async () => {
 	const { userId } = await auth();
@@ -9,18 +10,22 @@ const WishListProducts = async () => {
 
 	return (
 		<div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-			{likedProducts.map((product) => (
-				<div key={product.product.id}>
-					<span className="text-sm text-muted-foreground">
-						Liked on: {new Date(product.likedAt).toLocaleDateString()} at{" "}
-						{new Date(product.likedAt).toLocaleTimeString()}
-					</span>
-					<ProductCard
-						product={product.product}
-						initialLiked={likedIds.includes(product.product.id)}
-					/>
-				</div>
-			))}
+			{likedProducts.length > 0 ? (
+				likedProducts.map((product) => (
+					<div key={product.product.id}>
+						<span className="text-sm text-muted-foreground">
+							Liked on: {new Date(product.likedAt).toLocaleDateString()} at{" "}
+							{new Date(product.likedAt).toLocaleTimeString()}
+						</span>
+						<ProductCard
+							product={product.product}
+							initialLiked={likedIds.includes(product.product.id)}
+						/>
+					</div>
+				))
+			) : (
+				<EmptyState message="You have no liked products" />
+			)}
 		</div>
 	);
 };
